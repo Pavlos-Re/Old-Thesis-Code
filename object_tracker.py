c8027e62ef1f51942f343d4170874c7b99befea0
Pavlos-Re <pavlos.repin@gmail.com>
2023-04-28 15:21:32 +0300

    Commit

--- object_tracker.py
+++ object_tracker.py
@@ -69,7 +69,7 @@ def main(_argv):
 
     dis = []
     dis5 = []
-    dis3 = []
+    dis6 = []
 
     # initialize deep sort
     model_filename = 'model_data/mars-small128.pb'
@@ -129,7 +129,7 @@ def main(_argv):
             print('Video has ended or failed, try a different video format!')
             break
         frame_num += 1
-        #print('Frame #: ', frame_num)
+        # print('Frame #: ', frame_num)
         frame_size = frame.shape[:2]
         image_data = cv2.resize(frame, (input_size, input_size))
         image_data = image_data / 255.
@@ -210,7 +210,7 @@ def main(_argv):
         if FLAGS.count:
             cv2.putText(frame, "Objects being tracked: {}".format(count), (5, 35), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2,
                         (0, 255, 0), 2)
-            #print("Objects being tracked: {}".format(count))
+            # print("Objects being tracked: {}".format(count))
         # delete detections that are not in allowed_classes
         bboxes = np.delete(bboxes, deleted_indx, axis=0)
         scores = np.delete(scores, deleted_indx, axis=0)
@@ -262,42 +262,45 @@ def main(_argv):
             #                                                                                        int(bbox[1]),
             #                                                                                        int(bbox[2]),
             #                                                                                        int(bbox[3]))))
-
-        # To dis exei tis times twn suntetagmenwn tou kathe oxhmatos
-        # To dis2 exei tis times twn oxhmatwn pou isws exoun sugkroustei
-        # Sthn parakatw epanalhpsh sugkrinoume tis times tou neou dis me tis times tou palioy dis2 gia kathe ena apo ta oxhmata auta
-        # an kapoio apo auta exei thn idia timh paei na pei oti exei stamathsei na kinhte ara polu pithanon na exei ginei atuxhma
-
-        # if len(dis) > 0:
-        #    length = len(dis)
-        #    for i in range(length):
-        #         print("The result is: ", dis[i].to_tlbr(), " + ", dis[i].get_class(), " + ", dis[i].track_id)
-        #        if dis1[i].to_tlbr()[0] == dis[i].to_tlbr()[0] and dis1[i].to_tlbr()[1] == dis[i].to_tlbr()[1] and dis1[i].to_tlbr()[2] == dis[i].to_tlbr()[2] \
-        #                and dis1[i].to_tlbr()[3] == dis[i].to_tlbr()[3]:
-        #            print("Warming! ---> Possible collision", dis[i].track_id)
-
-        #        for y in range(length):
-        #            if (dis[i][0] == dis2[i][0]) and (dis[i][1] == dis2[i][1]) and (dis[i][2] == dis2[i][2]) and (dis[i][3] == dis2[i][3]):
-        #                print("Warning possible collision!", dis[i], " === ", dis2[i])
-
-        # dis2 = []
-        # dis3 = []
-        if frame_num == 30:
+        if frame_num % 10 == 0:
+#It is not updating values
+            if len(dis5) > 0:
+                for i in range(len(dis5)):
+                    for y in range(len(dis)):
+                        if (dis[y].track_id == dis5[i].track_id):
+                            print("Old value: ", dis5[i].to_tlbr())
+                            dis5[i] = dis[y]
+                            print("New value: ", dis5[i].to_tlbr())
+            if len(dis6) > 0:
+                for i in range(len(dis6)):
+                    for y in range(len(dis)):
+                        if (dis[y].track_id == dis6[i].track_id):
+                            print("Old value: ", dis6[i].to_tlbr())
+                            dis6[i] = dis[y]
+                            print("New value: ", dis6[i].to_tlbr())
+        if frame_num == 80:
             if len(dis5) > 0:
                 length = len(dis5)
                 for i in range(length):
                     for y in range(len(dis)):
                         if dis[y].track_id == dis5[i].track_id:
                             if (dis[y].to_tlbr() == dis5[i].to_tlbr()).all():
-                                print("Possible collision --> ", dis5[i].track_id)
-                                #dis[y].to_tlbr() - 10 , dis[y].to_tlbr()
+                                print("Possible collision ------> ", dis5[i].to_tlbr(), "  ", dis[y].to_tlbr(), "1) ",
+                                      dis5[i].track_id, "2) ", dis[y].track_id)
+            if len(dis6) > 0:
+                length = len(dis6)
+                for i in range(length):
+                    for y in range(len(dis)):
+                        if dis[y].track_id == dis6[i].track_id:
+                            if (dis[y].to_tlbr() == dis6[i].to_tlbr()).all():
+                                print("Possible collision ------> ", dis6[i].to_tlbr(), "  ", dis[y].to_tlbr(), "1) ",
+                                      dis6[i].track_id, "2) ", dis[y].track_id)
 
         ####### Coords = { xmin, ymin, xmax, ymax} #######
 
-
-        #Comparing the coords of each box's edge of the tracker B with the tracker A. With that way we can have an idea if
-        #one of the trackers is inside the other.
-        #Should that happen the list dis5 is being filled with the tracker's information.
+        # Comparing the coords of each box's edge of the tracker B with the tracker A. With that way we can have an
+        # idea if one of the trackers is inside the other. Should that happen the list dis5 is being filled with the
+        # tracker's information.
 
         if len(dis) > 0:
             length = len(dis)
@@ -319,6 +322,15 @@ def main(_argv):
                             elif len(dis5) == 0:
                                 dis5.append(dis[i])
 
+                            if len(dis6) > 0:
+                                for a in range(len(dis6)):
+                                    if dis6[a].track_id == dis[y].track_id:
+                                        exists = True
+                                if (exists == False):
+                                    dis6.append(dis[y])
+                            elif len(dis6) == 0:
+                                dis6.append(dis[y])
+
                         if (((dis[i].to_tlbr()[1] < dis[y].to_tlbr()[1]) and (
                                 dis[i].to_tlbr()[3] > dis[y].to_tlbr()[1])) and (
                                 (dis[i].to_tlbr()[0] < dis[y].to_tlbr()[0]) and (
@@ -332,6 +344,15 @@ def main(_argv):
                                     dis5.append(dis[i])
                             elif len(dis5) == 0:
                                 dis5.append(dis[i])
+
+                            if len(dis6) > 0:
+                                for a in range(len(dis6)):
+                                    if dis6[a].track_id == dis[y].track_id:
+                                        exists = True
+                                if (exists == False):
+                                    dis6.append(dis[y])
+                            elif len(dis6) == 0:
+                                dis6.append(dis[y])
 
                         if (((dis[i].to_tlbr()[1] < dis[y].to_tlbr()[3]) and (
                                 dis[i].to_tlbr()[3] > dis[y].to_tlbr()[3])) and (
@@ -347,6 +368,15 @@ def main(_argv):
                             elif len(dis5) == 0:
                                 dis5.append(dis[i])
 
+                            if len(dis6) > 0:
+                                for a in range(len(dis6)):
+                                    if dis6[a].track_id == dis[y].track_id:
+                                        exists = True
+                                if (exists == False):
+                                    dis6.append(dis[y])
+                            elif len(dis6) == 0:
+                                dis6.append(dis[y])
+
                         if (((dis[i].to_tlbr()[1] < dis[y].to_tlbr()[1]) and (
                                 dis[i].to_tlbr()[3] > dis[y].to_tlbr()[1])) and (
                                 (dis[i].to_tlbr()[0] < dis[y].to_tlbr()[2]) and (
@@ -361,14 +391,25 @@ def main(_argv):
                             elif len(dis5) == 0:
                                 dis5.append(dis[i])
 
+                            if len(dis6) > 0:
+                                for a in range(len(dis6)):
+                                    if dis6[a].track_id == dis[y].track_id:
+                                        exists = True
+                                if (exists == False):
+                                    dis6.append(dis[y])
+                            elif len(dis6) == 0:
+                                dis6.append(dis[y])
+
         dis = []
 
-        if frame_num == 30:
+        if frame_num == 80:
             frame_num = 0
+            dis5 = []
+            dis6 = []
 
         # calculate frames per second of running detections
         fps = 1.0 / (time.time() - start_time)
-        #print("FPS: %.2f" % fps)
+        # print("FPS: %.2f" % fps)
         result = np.asarray(frame)
         result = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
